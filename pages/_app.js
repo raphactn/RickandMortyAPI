b498b4e65cc39f23305e63ba3a5f29228d086643
raphactn <raphael.d.caetano48@gmail.com>
2022-08-07 19:54:59 -0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+// pages/_app.js
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const theme = extendTheme({
+    styles: {
+      global: (props) => ({
+        body: {
+          bg: 'black',
+          minWidth: '500px',
+          color: 'white'
+        }
+      })
+    },
+  })
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
